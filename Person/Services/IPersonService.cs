ec49751266f42242464492ee2568d16001be2652
lucascostasantiago <lcscostasantiago@gmail.com>
2022-06-15 00:07:55 -0300

    Implementing get, post, put and delete verbs.

---
+++ Person/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using PersonList.Model;
+using System.Collections.Generic;
+
+namespace PersonList.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+    }
+}
---
+++ Person/Startup.cs
@@ -0,0 +1,6 @@
+﻿namespace PersonList
+{
+    public class Startup
+    {
+    }
+}
